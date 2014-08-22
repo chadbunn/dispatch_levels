f49cb09cd7e357cb448b5d9d8de8a995a8e5c6cc
chadbunn <chadbunn@gmail.com>
2014-08-22 11:59:26 -0700

    Update coastal_latest_report_modtime.sql

--- coastal_latest_report_modtime.sql
+++ coastal_latest_report_modtime.sql
@@ -1,21 +1,18 @@
-ALTER TABLE coastal 
-    ADD latest_report TIMESTAMP;
- 
-ALTER TABLE coastal 
-    ALTER COLUMN latest_report 
-        SET DEFAULT CURRENT_TIMESTAMP;
- 
-UPDATE coastal
-    SET latest_report=CURRENT_TIMESTAMP;
- 
+ALTER TABLE coastal ADD latest_report TIMESTAMP;
+
+ALTER TABLE coastal ALTER COLUMN latest_report 
+SET DEFAULT CURRENT_TIMESTAMP;
+
+UPDATE coastal SET latest_report=CURRENT_TIMESTAMP;
+
 CREATE OR REPLACE FUNCTION coastal_latest_report()
-        RETURNS TRIGGER AS '
-  BEGIN
-    NEW.latest_report = NOW();
-    RETURN NEW;
-  END;
-' LANGUAGE 'plpgsql';
- 
+  RETURNS TRIGGER AS $$
+BEGIN
+   NEW.latest_report := current_timestamp at time zone 'PST';
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER coastal_latest_report_modtime BEFORE UPDATE
   ON coastal FOR EACH ROW EXECUTE PROCEDURE
   coastal_latest_report();
