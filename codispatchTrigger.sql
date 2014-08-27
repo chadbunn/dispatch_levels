7b71c89fb50ed07b85983a3a56dfe2055d4300bc
chadbunn <chadbunn@gmail.com>
2014-08-27 09:26:42 -0700

    added TEMP, RH, IC, SC, BI to query

--- codispatchTrigger.sql
+++ codispatchTrigger.sql
@@ -3,7 +3,7 @@ RETURNS trigger AS $$
 
 BEGIN
 
-UPDATE coastal SET ag_dispatch_level = obs.named_ag_dispatch_level, timestamp = obs.named_timestamp
+UPDATE coastal SET ag_dispatch_level = obs.named_ag_dispatch_level, timestamp = obs.named_timestamp, temperature = obs.named_ag_temperature, rh = obs.named_ag_relative_humidity, ic = obs.named_ag_ignition_component, sc = obs.named_ag_spread_component, bi = obs.named_ag_burning_index
 FROM obs
 WHERE obs.created_at = (SELECT MAX(created_at) FROM obs)
 AND coastal.cartodb_id = 1;
