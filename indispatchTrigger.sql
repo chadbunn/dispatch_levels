5257a0254b3c0e4808b9ecb51a59a798002b957d
chadbunn <chadbunn@gmail.com>
2014-08-27 09:28:31 -0700

    added TEMP, RH, IC, SC, BI to query

--- indispatchTrigger.sql
+++ indispatchTrigger.sql
@@ -3,7 +3,7 @@ RETURNS trigger AS $$
 
 BEGIN
 
-UPDATE inland SET lt_dispatch_level = obs.named_lt_dispatch_level, timestamp = obs.named_timestamp
+UPDATE inland SET lt_dispatch_level = obs.named_lt_dispatch_level, timestamp = obs.named_timestamp, temperature = obs.named_lt_temperature, rh = obs.named_lt_relative_humidity, ic = obs.named_lt_ignition_component, sc = obs.named_lt_spread_component, bi = obs.named_lt_burning_index
 FROM obs
 WHERE obs.created_at = (SELECT MAX(created_at) FROM obs)
 AND inland.cartodb_id = 1;
