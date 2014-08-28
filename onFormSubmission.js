03d339f39730eabbca35c8ade83959e8d033d6de
chadbunn <chadbunn@gmail.com>
2014-08-28 09:54:54 -0700

    added report as variable

--- onFormSubmission.js
+++ onFormSubmission.js
@@ -25,6 +25,7 @@ function onFormSubmission(e) {
     e.namedValues.ag_spread_component[0],
     e.namedValues.ag_burning_index[0],
     e.namedValues.ag_dispatch_level[0],
+    e.namedValues.report[0],
     e.namedValues.timestamp[0]
   );
 }
@@ -32,7 +33,7 @@ function onFormSubmission(e) {
 /**
  * Insert color into CartoDB
  */
-function postToCartoDB(lt_temperature,lt_relative_humidity,lt_ignition_component,lt_spread_component,lt_burning_index,lt_dispatch_level,lp_temperature,lp_relative_humidity,lp_ignition_component,lp_spread_component,lp_burning_index,lp_dispatch_level,ag_temperature,ag_relative_humidity,ag_ignition_component,ag_spread_component,ag_burning_index,ag_dispatch_level,timestamp
+function postToCartoDB(lt_temperature,lt_relative_humidity,lt_ignition_component,lt_spread_component,lt_burning_index,lt_dispatch_level,lp_temperature,lp_relative_humidity,lp_ignition_component,lp_spread_component,lp_burning_index,lp_dispatch_level,ag_temperature,ag_relative_humidity,ag_ignition_component,ag_spread_component,ag_burning_index,ag_dispatch_level,report,timestamp
 ) {
   Logger.log("posting to CartoDB");
   
@@ -63,11 +64,12 @@ function postToCartoDB(lt_temperature,lt_relative_humidity,lt_ignition_component
   ag_spread_component = ag_spread_component.replace("'","''");
   ag_burning_index = ag_burning_index.replace("'","''");
   ag_dispatch_level = ag_dispatch_level.replace("'","''");
+  report = report.replace("'","''");
   timestamp = timestamp.replace("'","''");
   /**
    * Here is the INSERT statement
    */
-  var query = "INSERT INTO obs(named_lt_temperature,named_lt_relative_humidity,named_lt_ignition_component,named_lt_spread_component,named_lt_burning_index,named_lt_dispatch_level,named_lp_temperature,named_lp_relative_humidity,named_lp_ignition_component,named_lp_spread_component,named_lp_burning_index,named_lp_dispatch_level,named_ag_temperature,named_ag_relative_humidity,named_ag_ignition_component,named_ag_spread_component,named_ag_burning_index,named_ag_dispatch_level,named_timestamp,the_geom) VALUES('"+lt_temperature.replace(/'/g, "''")+"','"+lt_relative_humidity.replace(/'/g, "''")+"','"+lt_ignition_component.replace(/'/g, "''")+"','"+lt_spread_component.replace(/'/g, "''")+"','"+lt_burning_index.replace(/'/g, "''")+"','"+lt_dispatch_level.replace(/'/g, "''")+"','"+lp_temperature.replace(/'/g, "''")+"','"+lp_relative_humidity.replace(/'/g, "''")+"','"+lp_ignition_component.replace(/'/g, "''")+"','"+lp_spread_component.replace(/'/g, "''")+"','"+lp_burning_index.replace(/'/g, "''")+"','"+lp_dispatch_level.replace(/'/g, "''")+"','"+ag_temperature.replace(/'/g, "''")+"','"+ag_relative_humidity.replace(/'/g, "''")+"','"+ag_ignition_component.replace(/'/g, "''")+"','"+ag_spread_component.replace(/'/g, "''")+"','"+ag_burning_index.replace(/'/g, "''")+"','"+ag_dispatch_level.replace(/'/g, "''")+"','"+timestamp.replace(/'/g, "''")+"',"+null+")";
+  var query = "INSERT INTO obs(named_lt_temperature,named_lt_relative_humidity,named_lt_ignition_component,named_lt_spread_component,named_lt_burning_index,named_lt_dispatch_level,named_lp_temperature,named_lp_relative_humidity,named_lp_ignition_component,named_lp_spread_component,named_lp_burning_index,named_lp_dispatch_level,named_ag_temperature,named_ag_relative_humidity,named_ag_ignition_component,named_ag_spread_component,named_ag_burning_index,named_ag_dispatch_level,named_report,named_timestamp,the_geom) VALUES('"+lt_temperature.replace(/'/g, "''")+"','"+lt_relative_humidity.replace(/'/g, "''")+"','"+lt_ignition_component.replace(/'/g, "''")+"','"+lt_spread_component.replace(/'/g, "''")+"','"+lt_burning_index.replace(/'/g, "''")+"','"+lt_dispatch_level.replace(/'/g, "''")+"','"+lp_temperature.replace(/'/g, "''")+"','"+lp_relative_humidity.replace(/'/g, "''")+"','"+lp_ignition_component.replace(/'/g, "''")+"','"+lp_spread_component.replace(/'/g, "''")+"','"+lp_burning_index.replace(/'/g, "''")+"','"+lp_dispatch_level.replace(/'/g, "''")+"','"+ag_temperature.replace(/'/g, "''")+"','"+ag_relative_humidity.replace(/'/g, "''")+"','"+ag_ignition_component.replace(/'/g, "''")+"','"+ag_spread_component.replace(/'/g, "''")+"','"+ag_burning_index.replace(/'/g, "''")+"','"+ag_dispatch_level.replace(/'/g, "''")+"','"+report.replace(/'/g, "''")+"','"+timestamp.replace(/'/g, "''")+"',"+null+")";
 
   Logger.log("SQL: "+query);  
 
